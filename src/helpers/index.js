42c8261c9abd030187dbe652fac160757def69c3
Serhei Honcharov <mrsedmor@gmail.com>
2023-05-15 16:21:43 +0300

    feat: add validation and change birthday type to Date

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,9 +1,11 @@
 const HttpError = require('./HttpError');
 const ctrlWrapper = require('./ctrlWrapper');
 const authenticateBearer = require('./authenticateBearer');
+const handleMongooseError = require('./handleMongooseError');
 
 module.exports = {
   HttpError,
   ctrlWrapper,
   authenticateBearer,
+  handleMongooseError,
 };
--- src/models/notice.js
+++ src/models/notice.js
@@ -1,10 +1,15 @@
 const { Schema, model } = require('mongoose');
 const Joi = require('joi');
+const handleMongooseError = require('../helpers/handleMongooseError');
 
+const TITLE_MIN_LENGTH = 4;
+const TITLE_MAX_LENGTH = 60;
 const NAME_MIN_LENGTH = 2;
 const NAME_MAX_LENGTH = 16;
 const BREED_MIN_LENGTH = 2;
 const BREED_MAX_LENGTH = 16;
+const COMMENTS_MIN_LENGTH = 0;
+const COMMENTS_MAX_LENGTH = 120;
 
 const NOTICE_CATEGORIES = {
   sell: 'sell',
@@ -38,12 +43,12 @@ const noticeSchema = new Schema(
       required: [true, 'Name is required'],
     },
     birthday: {
-      type: String,
+      type: Date,
       required: [true, 'Birthday is required'],
     },
     breed: {
       type: String,
-      default: '',
+      required: [true, 'Breed is required'],
     },
     photoUrl: {
       type: String,
@@ -52,16 +57,18 @@ const noticeSchema = new Schema(
     sex: {
       type: String,
       enum: SEX_LIST,
+      required: [true, 'Pet gender is required'],
     },
     location: {
       type: String,
-      default: '',
+      required: [true, 'Pet location is required'],
     },
     price: {
       type: Number,
     },
     comments: {
       type: String,
+      default: '',
     },
     favorites: {
       type: [{ type: Schema.Types.ObjectId, ref: 'users' }],
@@ -71,6 +78,8 @@ const noticeSchema = new Schema(
   { versionKey: false, timestamps: true }
 );
 
+noticeSchema.post('save', handleMongooseError);
+
 const Notice = model('notices', noticeSchema);
 
 // Validation schemas:
@@ -79,11 +88,9 @@ const addParams = Joi.object({
   category: Joi.string()
     .required()
     .valid(...NOTICE_CATEGORIES_LIST),
-  title: Joi.string().required(),
+  title: Joi.string().min(TITLE_MIN_LENGTH).max(TITLE_MAX_LENGTH).required(),
   name: Joi.string().required().min(NAME_MIN_LENGTH).max(NAME_MAX_LENGTH),
-  birthday: Joi.string()
-    .required()
-    .pattern(/^\d{2}.\d{2}.\d{4}$/),
+  birthday: Joi.date().iso().less('now').required(),
   breed: Joi.string().required().min(BREED_MIN_LENGTH).max(BREED_MAX_LENGTH),
   sex: Joi.string()
     .required()
@@ -92,8 +99,9 @@ const addParams = Joi.object({
   price: Joi.number().integer().positive().when('category', {
     is: NOTICE_CATEGORIES.sell,
     then: Joi.required(),
+    otherwise: Joi.forbidden(),
   }),
-  comments: Joi.string().min(8).max(120),
+  comments: Joi.string().min(COMMENTS_MIN_LENGTH).max(COMMENTS_MAX_LENGTH),
 });
 
 const getParams = Joi.object({
@@ -103,8 +111,8 @@ const getParams = Joi.object({
   location: Joi.string(),
   favorite: Joi.boolean(),
   own: Joi.boolean(),
-  page: Joi.number().integer().positive(),
-  limit: Joi.number().integer().positive(),
+  page: Joi.number().integer().min(1),
+  limit: Joi.number().integer().min(1).max(100),
 });
 
 const updateFavoriteParams = Joi.object({
