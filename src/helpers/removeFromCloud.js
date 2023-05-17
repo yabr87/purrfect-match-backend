4369e2cded955dceb79a3cb8c2018a4f91e394e9
Serhei Honcharov <mrsedmor@gmail.com>
2023-05-17 11:30:18 +0300

    fix: remove congole.log

--- src/helpers/removeFromCloud.js
+++ src/helpers/removeFromCloud.js
@@ -4,12 +4,12 @@ const removeFromCloud = fileUrl => {
   if (!fileUrl.includes('cloudinary.com')) {
     return;
   }
-  console.log(fileUrl);
+
   const versionIndex = fileUrl.search(/\/v\d/);
   const fileId = fileUrl
     .slice(fileUrl.indexOf('/', versionIndex + 1) + 1)
     .split('.')[0];
-  console.log(fileId);
+
   return cloudinary.uploader.destroy(fileId);
 };
 
